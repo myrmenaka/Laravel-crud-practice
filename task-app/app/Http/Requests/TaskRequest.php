504430192a46ce762ac65b98d7335f8fe467e5f9
myrmenaka <akamaru.0326@outlook.com>
2026-02-15 11:33:51 +0900

    FormRequest

---
+++ task-app/app/Http/Requests/TaskRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+
+            // 既存
+            'category_id' => ['nullable', 'exists:categories,id'],
+
+            // 新規
+            'new_category' => ['nullable', 'string', 'max:50', 'unique:categories,name'],
+        ];
+    }
+}
